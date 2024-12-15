c4ac2b672c2ebfccc898835703f52a3c95ff7fb0
whowouldwin <leranetwork@gmail.com>
2024-12-16 00:26:00 +0300

    feat: implement getCardId

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -519,8 +519,28 @@ function encodeToRot13(str) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const ranks = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+  const suits = ['♣', '♦', '♥', '♠'];
+  const rank = value.slice(0, -1);
+  const suit = value.slice(-1);
+  const suitIndex = suits.indexOf(suit);
+  const rankIndex = ranks.indexOf(rank);
+  return suitIndex * 13 + rankIndex;
 }
 
 module.exports = {
