a3df03134fffa8879919ee95470b429322850694
whowouldwin <leranetwork@gmail.com>
2024-12-15 19:26:17 +0300

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -369,8 +369,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const strSplit = str.split(' ');
+  const reversedArray = strSplit.map((word) =>
+    word.split('').reverse().join('')
+  );
+  return reversedArray.join(' ');
 }
 
 /**
