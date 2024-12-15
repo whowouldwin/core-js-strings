a59c1a0d31663e73a087ed4b40d5af62aa382371
whowouldwin <leranetwork@gmail.com>
2024-12-15 19:41:13 +0300

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -388,8 +388,16 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((char) => {
+      if (char === char.toUpperCase()) {
+        return char.toLowerCase();
+      }
+      return char.toUpperCase();
+    })
+    .join('');
 }
 
 /**
